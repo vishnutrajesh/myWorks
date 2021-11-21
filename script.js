e863b8fb8886afacf5ab194bf5c7e6d68bc6933a
vishnutrajesh <vishnu.email007@gmail.com>
2021-11-21 21:04:45 +0530

    works page 75%

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const active = document.getElementsByClassName('active-block');
 const nav = document.getElementById('myNav');
 const links = nav.getElementsByTagName('a');
 const socialNav = document.getElementsByClassName('social-nav');
+let techUsed = [
+    {tech: ['Angular', 'Fire Base', 'Javascript', 'SCSS | CSS', 'HTML'], resposive: true},
+    {tech: ['Javascript', 'THREE JS', 'SCSS | CSS', 'HTML'], resposive: true},
+    {tech: ['Angular', 'Javascript', 'E-Charts', 'SCSS | CSS', 'HTML'], resposive: false},
+    {tech: ['Javascript', 'Bootstrap', 'SCSS | CSS', 'HTML'], resposive: true},
+    {tech: ['Javascript', 'Bootstrap', 'SCSS | CSS', 'HTML'], resposive: true},
+]
 // const links = nav.getElementsByTagName("a");
 // for (let link of links) {
 //     link.addEventListener('mouseover',() => {
@@ -20,6 +27,7 @@ const svgLine = document.getElementById('svgLine');
 const introText = document.getElementById('introText');
 const introSpan = introText.getElementsByTagName('span')
 const piller = pillerContainer[0].getElementsByClassName('piller')
+const wrapper = document.getElementsByClassName('card-wrapper');
 let blockUnresponsiveEvent = true;
 
 //navigation
@@ -27,10 +35,80 @@ const skill = document.getElementById('skills');
 const aboutMe = document.getElementById('aboutMe');
 const works = document.getElementById('works');
 const contact = document.getElementById('contact');
+const workCard = document.getElementsByClassName('work-card');
+const popup = document.getElementsByClassName('my-modal');
+let bg = ["./img/drag-n-drop.png",
+    "./img/coming-soon-singularity.png",
+    "./img/covid-chart.png",
+    "./img/scroll-lp.png",
+    "./img/app-lp.png]"];
+
 for(let span of introSpan) {
     setInterval(() => {
         span.classList.add('pop');
     }, 300)
+}
+function expand(index) {
+    workCard[index].querySelector('.navigate').classList.add('animated');
+    if(workCard[index].querySelector('.expand') !== null) {
+        workCard[index].querySelector('.expand').classList.add('animated');   
+    }
+    // setTimeout(() => {
+    //     popup[0].appendChild(workCard[index].cloneNode(true));
+    //     let card = popup[0].querySelector('.work-card');
+    //     card.style.width = width + 'px';
+    //     card.style.height = height + 'px';
+    //     popup[0].classList.add('d-block');
+    // }, 100)
+}
+function openPanel(index) {
+    workCard[index].querySelector('.expand').classList.remove('animated');
+    workCard[index].querySelector('.navigate').classList.remove('animated');
+    popup[0].innerHTML = null;
+    popup[0].style.display = 'block';
+    const height = workCard[index].clientHeight;
+    const width = workCard[index].clientWidth;
+    popup[0].appendChild(wrapper[index].querySelector('.work-card').cloneNode(true));
+    let card = popup[0].querySelector('.work-card');
+    card.style.backgroundImage = `url(${bg[index]})`
+    let innerEl1 = card.querySelector('.navigate');
+    let innerEl2 = card.querySelector('.expand');
+    innerEl2.remove();
+    innerEl1.remove();
+    console.log(card);
+    card.classList.remove('h-100');
+    card.classList.remove('position-relative');
+    card.style.width = width + 'px';
+    card.style.height = height + 'px';
+    card.style.position = 'fixed';
+    let rec = wrapper[index].getBoundingClientRect();
+    card.style.top = rec.top + window.scrollY + 'px';
+    card.style.left = rec.left + window.scrollX + 'px';
+    let parentDiv  = works.getBoundingClientRect();
+    setTimeout(() => {
+        card.style.top = parentDiv.top + window.scrollY + 'px';
+        card.style.left = parentDiv.left + window.scrollX + 'px';  
+        card.style.width = works.clientWidth + 'px';  
+        card.style.height = works.clientHeight + 'px';
+    }, 1000)
+    setTimeout(() => {
+        card.style.backgroundImage = null;
+        card.innerHTML = `
+        <div class="row h-100 position-relative align-items-center m-0">
+        <div class="close-btn"><i class="fas fa-times"></i></div>
+        <div class="col-2"></div>
+        <div class="col-10 p-4">
+        <img src="${bg[index]}" alt="works" class="img-fluid">
+        </div>
+        </div>
+        `;
+    }, 2000)
+}
+function collapse(index) {
+    workCard[index].querySelector('.navigate').classList.remove('animated');
+    if(workCard[index].querySelector('.expand') !== null) {
+        workCard[index].querySelector('.expand').classList.remove('animated');
+    }
 }
 function navActive(link, i) {
     if(i===0){
